b67c2115881c2492ed27e6650a7e17b4d8539f5f
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 13:26:07 +0300

    Initial commit

---
+++ BiggestNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿class BiggestNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int biggestNumber = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int currentNumber = int.Parse(Console.ReadLine());
+            if (currentNumber > biggestNumber)
+            {
+                biggestNumber = currentNumber;
+            }
+        }
+
+        Console.WriteLine(biggestNumber);
+    }
+}
